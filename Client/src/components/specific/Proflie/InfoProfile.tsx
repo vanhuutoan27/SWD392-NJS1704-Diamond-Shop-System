add363162b6922cb5e61b8c38bdf391e6881b74a
KhaiToir <148189641+KhaiToir@users.noreply.github.com>
2024-05-22 00:06:03 +0700

    User-Profile-Page

--- Client/src/components/specific/Proflie/InfoProfile.tsx
+++ Client/src/components/specific/Proflie/InfoProfile.tsx
@@ -3,10 +3,10 @@ function InfoProfile() {
     <div className="container">
       <div className="flex justify-center text-sm font-medium">About me</div>
       <div className="mt-2 flex justify-center">
-        <p className="w-4/5 text-center text-xs font-light text-secondary">
+        <p className="w-4/5 text-center text-xs font-normal text-secondary">
           The web manager for DIAMOON, a leading online destination for
           high-quality diamonds. With extensive experience in web development
-          and e-commerce, I am dedicated to creating an exceptional online
+          and ecommerce, I am dedicated to creating an exceptional online
           shopping experience for our customers.
         </p>
       </div>
