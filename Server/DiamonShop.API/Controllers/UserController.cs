cbd9d2d3c2f48a758689c9dfedb3a5f02f351e3f
QuocDai <145921869+dainq94@users.noreply.github.com>
2024-05-30 11:45:32 +0700

    add api delete user - enable user (api update have some error need to check again )

--- Server/DiamonShop.API/Controllers/UserController.cs
+++ Server/DiamonShop.API/Controllers/UserController.cs
@@ -11,26 +11,28 @@ namespace DiamonShop.API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        ResultModel resp;
+
         private readonly UserManager<AppUser> _userManager;
         public UserController(UserManager<AppUser> userManager)
         {
             this._userManager = userManager;
+            resp = new ResultModel();
         }
         [HttpGet(Name = "GetUsers")]
         public async Task<ActionResult<ResultModel>> GetUsers()
         {
-            ResultModel resultModel = new ResultModel();
             var users = _userManager.Users.ToList();
             if (users is null)
             {
                 //resultModel.IsSuccess = false;
                 //resultModel.
             }
-            resultModel.IsSuccess = true;
-            resultModel.Message = "Succesful";
-            resultModel.Code = (int)HttpStatusCode.OK;
-            resultModel.Data = users;
-            return resultModel;
+            resp.IsSuccess = true;
+            resp.Message = "Succesful";
+            resp.Code = (int)HttpStatusCode.OK;
+            resp.Data = users;
+            return resp;
         }
 
         [HttpPost]
@@ -45,6 +47,7 @@ namespace DiamonShop.API.Controllers
                 Email = request.Email,
                 PhoneNumber = request.Phone,
                 UserName = request.FullName.Replace(" ", ""),
+                Address = request.Address,
                 IsActive = true,
                 SecurityStamp = Guid.NewGuid().ToString(),
                 LockoutEnabled = false,
@@ -65,5 +68,88 @@ namespace DiamonShop.API.Controllers
             resp.Code = (int)HttpStatusCode.OK;
             return resp;
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ResultModel>> UpdateUser(Guid id, [FromBody] UpdateUserRequest request)
+        {
+            if (!ModelState.IsValid || id == null) { return BadRequest(ModelState); }
+            var UserToEdit = await _userManager.FindByIdAsync(id.ToString());
+            if (UserToEdit == null) { return NotFound(ModelState); }
+
+
+            var roles = await _userManager.GetRolesAsync(UserToEdit);
+            var currenRole = roles.FirstOrDefault();
+            if (currenRole != request.Role && roles.Any())
+            {
+                var removeResult = await _userManager.RemoveFromRolesAsync(UserToEdit, roles);
+                if (!removeResult.Succeeded) return BadRequest($"Failed to remove role: {string.Join(", ", removeResult.Errors.Select(e => e.Description))}");
+
+                var currentRole = roles.FirstOrDefault().ToString();
+                var adddResult = await _userManager.AddToRoleAsync(UserToEdit, request.Role);
+            }
+
+
+            if (UserToEdit.Email != request.Email)
+                UserToEdit.Email = request.Email;
+
+            if (UserToEdit.FullName != request.FullName)
+            {
+                UserToEdit.FullName = request.FullName;
+                UserToEdit.UserName = request.FullName.Replace(" ", "");
+            }
+
+            if (UserToEdit.PhoneNumber != request.Phone)
+                UserToEdit.PhoneNumber = request.Phone;
+
+            if (UserToEdit.Address != request.Address)
+                UserToEdit.Address = request.Address;
+
+            var result = await _userManager.UpdateAsync(UserToEdit);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+                return BadRequest(ModelState);
+            }
+            resp.IsSuccess = true;
+            resp.Message = "Successfull";
+            resp.Code = (int)HttpStatusCode.OK;
+            return resp;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ResultModel>> DeleteUser(Guid id)
+        {
+            if (id == null) return BadRequest(ModelState);
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            if (user == null) return NotFound();
+            user.LockoutEnabled = true;
+            user.IsActive = false;
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded) { return BadRequest(result.Errors); }
+            resp.IsSuccess = true;
+            resp.Message = "Successfull";
+            resp.Code = (int)HttpStatusCode.OK;
+            return resp;
+        }
+
+        [HttpPut("enable-user/{id}")]
+        public async Task<ActionResult<ResultModel>> EnableUser(Guid id)
+        {
+            if (id == null) return BadRequest(ModelState);
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            if (user == null) return NotFound();
+            user.LockoutEnabled = false;
+            user.IsActive = true;
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded) { return BadRequest(result.Errors); }
+            resp.IsSuccess = true;
+            resp.Message = "Successfull";
+            resp.Code = (int)HttpStatusCode.OK;
+            return resp;
+
+        }
+
+
     }
 }
---
+++ Server/DiamonShop.Core/Models/content/RequestModels/UpdateUserRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace DiamonShop.Core.Models.content.RequestModels
+{
+    public class UpdateUserRequest
+    {
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        // public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
