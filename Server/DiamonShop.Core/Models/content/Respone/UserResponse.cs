6dc313a36d95f2d3b55b3560ba165bdd83a943db
QuocDai <145921869+dainq94@users.noreply.github.com>
2024-05-31 08:35:32 +0700

    add get userbyid and change status api

---
+++ Server/DiamonShop.Core/Models/content/Respone/UserResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace DiamonShop.Core.Models.content.Respone
+{
+    public class UserResponse
+    {
+        public Guid id { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? Dob { get; set; }
+        public string? Address { get; set; }
+        public string? Avatar { get; set; }
+        public DateTime? VipStartDate { get; set; }
+        public DateTime? VipExpireDate { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+        public double Balance { get; set; }
+        public double RoyaltyAmountPerPost { get; set; }
+        public IList<string> Roles { get; set; }
+
+    }
+}
