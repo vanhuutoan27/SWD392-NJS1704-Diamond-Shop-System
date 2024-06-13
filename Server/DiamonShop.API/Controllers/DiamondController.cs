58c98e291e3dce78fb4bb4401697de4f4a69f589
QuocDai <145921869+dainq94@users.noreply.github.com>
2024-06-13 15:02:13 +0700

    edit authenservice

--- Server/DiamonShop.API/Controllers/DiamondController.cs
+++ Server/DiamonShop.API/Controllers/DiamondController.cs
@@ -1,6 +1,7 @@
 ﻿using DiamonShop.Core.Models.content.RequestModels;
 using DiamonShop.Core.Models.content.Respone;
 using DiamonShop.Core.services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DiamonShop.API.Controllers
@@ -16,7 +17,7 @@ namespace DiamonShop.API.Controllers
             _services = services;
         }
         [HttpGet]
-
+        [Authorize]
         public async Task<IActionResult> GetAll()
         {
 
