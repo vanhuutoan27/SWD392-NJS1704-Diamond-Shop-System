36c8c550e76a12e8cdc081f377931e6d16a9982e
KhaiToir <148189641+KhaiToir@users.noreply.github.com>
2024-06-21 19:46:11 +0700

    fix: fix client when run server

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -28,7 +28,7 @@ function App() {
   const Promotion = lazy(() => import("./pages/Guest/Promotion/PromotionPage"))
   const Contact = lazy(() => import("./pages/Guest/Contact/ContactPage"))
   const Cart = lazy(() => import("./pages/Customer/Cart/MyCartPage"))
-  const Order = lazy(() => import("./pages/Customer/Order/OrderPage"))
+  const Order = lazy(() => import("./pages/Customer/Order/MyOrderPage"))
   const MyProfile = lazy(() => import("./pages/Customer/Profile/MyProfilePage"))
   const MySetting = lazy(() => import("./pages/Customer/Setting/MySettingPage"))
   const MyOrder = lazy(() => import("./pages/Customer/Order/MyOrderPage"))
